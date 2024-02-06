b4b6cdc12d4406656643db5150fa1b8f7c7ed8cb
anandupa2001 <upadhyay.anand2001@gmail.com>
2024-02-06 19:12:33 +0530

    add some fuctionality

--- usersearch/index.js
+++ usersearch/index.js
@@ -1,25 +1,25 @@
 let users = [];
 document.getElementById("search-btn").addEventListener("click", function () {
   let textbox = document.getElementById("textbox").value;
+  let filteredData = [...users];
   if (textbox?.trim() == "") {
-    alert("Please enter name");
+    alert("Please enter user name");
     fetchUserData();
   } else {
-    users = users.filter((value) => {
-      return value.firstName?.toLowerCase() === textbox?.toLowerCase();
+    filteredData = users.filter((value) => {
+      return value.id == textbox;
     });
   }
-  createBox();
-  console.log(users);
+  createBox(filteredData);
 });
 
 document.getElementById("textbox").addEventListener("keydown", function (e) {
+  let filteredData = [...users];
   if (e.target.value?.trim() != "" && e.code == "Enter" && e.key == "Enter") {
-    users = users.filter((value) => {
-      return value.firstName?.toLowerCase() === e.target.value?.toLowerCase();
+    filteredData = users.filter((value) => {
+      return value.id == e.target.value;
     });
-    createBox();
-    console.log(users);
+    createBox(filteredData);
   }
 });
 
@@ -28,32 +28,63 @@ function fetchUserData() {
     .then((response) => response.json())
     .then((res) => {
       users = res.users;
-      createBox();
+      createBox(users);
     });
 }
-
-function createBox() {
+function createBox(allUsers) {
   document.getElementById("user-box").innerHTML = "";
-  if (users.length === 0) {
-    document.getElementById("user-box").innerHTML = "<h1>No user found</h1>";
+  if (allUsers.length === 0) {
+    document.getElementById("user-box").innerHTML = "<h1> No user found</h1>";
   }
-  users.forEach((value) => {
+  allUsers.forEach((value) => {
     let box = document.createElement("div");
     box.classList.add("box");
-    console.log("hello");
-    box.innerHTML = `<div class="boxes">
+    box.innerHTML = `<div class="boxes" onclick>
         <img
           class="image"
           src= ${value.image}
           alt="image"
         />
+        <h3>Id : ${value.id}</h3>
         <h3>Name : ${value.firstName} ${value.lastName}</h3>
         <h3>Age : ${value.age}</h3>
         <h3>E-mail : ${value.email}</h3>
         <h3>Phone no. ${value.phone}</h3>
       </div>`;
+    box.addEventListener("click", () => callUserData(value.id));
     document.getElementById("user-box").append(box);
   });
 }
-
 fetchUserData();
+
+function callUserData(id) {
+  fetch(`https://dummyjson.com/users/${id}`)
+    .then((response) => response.json())
+    .then((res) => {
+      removeOtherImages(res);
+    });
+}
+
+function removeOtherImages(value) {
+  document.getElementById("user-box").innerHTML = "";
+  let box1 = document.createElement("div");
+  box1.classList.add("box");
+  box1.innerHTML = `<button class="btn" onclick="createBox(users)"><img class="back-icon" src="https://cdn-icons-png.flaticon.com/128/7710/7710436.png" /></button><div class="user-box">
+        <img
+          class="user-image"
+          src= ${value.image}
+          alt="image"
+        />
+        <h3>Id : ${value.id}</h3>
+        <h3>Name : ${value.firstName} ${value.lastName}</h3>
+        <h3>Gender : ${value.gender}</h3>
+        <h3>Age : ${value.age}</h3>
+        <h3>Birth Date : ${value.birthDate}</h3>
+        <h3>Blood Group : ${value.bloodGroup}</h3>
+        <h3>E-mail : ${value.email}</h3>
+        <h3>Password : ${value.password}</h3>
+        <h3>Phone no. ${value.phone}</h3>
+        <h3>Address : ${value.address.address}, ${value.address.city}, ${value.address.state}</h3>
+      </div>`;
+  document.getElementById("user-box").append(box1);
+}
